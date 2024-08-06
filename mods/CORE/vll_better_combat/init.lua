8aa58451f969dbf85815cc8391750321db1a7f5a
payton <pdp9729@gmail.com>
2024-08-06 11:34:40 -0600

    Stub in default_attack_table

--- mods/CORE/vll_better_combat/init.lua
+++ mods/CORE/vll_better_combat/init.lua
@@ -1,5 +1,20 @@
 vll_better_combat = {}
 
-function vll_better_combat.register_melee(name, details)
+vll_better_combat.default_attack_table = {}
+vll_better_combat.default_attack_table.directional_offensive = function(pos1, pos2)
+	-- TODO
+end
+vll_better_combat.default_attack_table.inward_offensive = function(dir)
+	-- TODO
+end
+vll_better_combat.default_attack_table.directional_defensive = function(pos1, pos2)
+	-- TODO
+end
+vll_better_combat.default_attack_table.inward_defensive = function(dir)
+	-- TODO
+end
+
+function vll_better_combat.register_melee(name, details, attack_table)
+	if attack_table == nil then attack_table = vll_better_combat.default_attack_table
 	minetest.register_tool(name, details)
 end
